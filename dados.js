afc5cabfc63057fc83926a36313f6372b7eb7489
Ednei-Costa051072 <costa.ednei@gmail.com>
2024-09-09 10:43:32 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal é uma skatista brasileira, conhecida como a Fada do Skate. Rayssa Leal é uma skatista brasileira, conhecida como a Fada do Skate.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "skate fada sk8"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira, conhecida por sua técnica e determinação....Beatriz Souza é uma judoca brasileira, conhecida por sua técnica e determinação....",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza_judo",
+        tags : "judô judo judoca judoka"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma das maiores ginastas da história o Brasil. Com um talento inegável e uma determinação impressionante, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "ginástica ginastica ouro"
+    }
+   ];
